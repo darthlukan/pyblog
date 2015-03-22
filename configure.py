5a55f7fa7cf6235d47de9057952cc6a196aab2be
Brian Tomlinson <darthlukan@gmail.com>
2015-03-22 17:01:10 +0100

    TODO: Restruct and offload to pyblog.py

--- configure.py
+++ configure.py
@@ -24,49 +24,49 @@ import subprocess
 
 from time import sleep
 
-
-color = {
-    'Blue': '\033[94m',
-    'Green': '\033[92m',
-    'Yellow': '\033[93m',
-    'Bold': '\033[1m',
-    'End': '\033[0m'
-}
-
-
-def detect_user():
-    if os.geteuid() == 0:
-        print "%s You are root continuing! %s" % (color['Bold'], color['End'])
-        return True
-    print "%s %s PyBlog configuration needs to be run as root! Sorry! %s" % (color['Bold'], color['Yellow'], color['End'])
-    return False
-
-
-def define_blog():
-    blogdir = raw_input("%s What is the main directory of your blog? ex: [/var/www/myblog] %s" % (color['Bold'], color['End']))
-    blogtitle = raw_input("%s What is the title of your blog? ex: My Cool Blog %s " % (color['Bold'], color['End']))
-    return blogdir, blogtitle
-
-
-def detect_webserver():
-    webserver = None
-    servers = {
-        'apache': commands.getoutput('which apache2'),
-        'nginx': commands.getoutput('which nginx'),
-        'lighthttpd': commands.getoutput('which lighttpd')
-    }
-    print "%sThis program relies on a webserver such as Apache, Nginx, or Lighttpd.%s" % (color['Bold'], color['End'])
-
-    for server, val in servers.items():
-        if "which: no" not in val:
-            print "%s%s found, continuing" % (color['Bold'], server)
-            webserver = server
-            break
-        else:
-            print "No webserver found, please install either Apache2, Nginx, or Lighttpd to continue %s" % color['End']
-            continue
-
-    return webserver
+# TODO: Offload all of this to the pyblog file or other libs as needed.
+# color = {
+#     'Blue': '\033[94m',
+#     'Green': '\033[92m',
+#     'Yellow': '\033[93m',
+#     'Bold': '\033[1m',
+#     'End': '\033[0m'
+# }
+#
+#
+# def detect_user():
+#     if os.geteuid() == 0:
+#         print "%s You are root continuing! %s" % (color['Bold'], color['End'])
+#         return True
+#     print "%s %s PyBlog configuration needs to be run as root! Sorry! %s" % (color['Bold'], color['Yellow'], color['End'])
+#     return False
+#
+#
+# def define_blog():
+#     blogdir = raw_input("%s What is the main directory of your blog? ex: [/var/www/myblog] %s" % (color['Bold'], color['End']))
+#     blogtitle = raw_input("%s What is the title of your blog? ex: My Cool Blog %s " % (color['Bold'], color['End']))
+#     return blogdir, blogtitle
+#
+#
+# def detect_webserver():
+#     webserver = None
+#     servers = {
+#         'apache': commands.getoutput('which apache2'),
+#         'nginx': commands.getoutput('which nginx'),
+#         'lighthttpd': commands.getoutput('which lighttpd')
+#     }
+#     print "%sThis program relies on a webserver such as Apache, Nginx, or Lighttpd.%s" % (color['Bold'], color['End'])
+#
+#     for server, val in servers.items():
+#         if "which: no" not in val:
+#             print "%s%s found, continuing" % (color['Bold'], server)
+#             webserver = server
+#             break
+#         else:
+#             print "No webserver found, please install either Apache2, Nginx, or Lighttpd to continue %s" % color['End']
+#             continue
+#
+#     return webserver
 
 
 def main():
